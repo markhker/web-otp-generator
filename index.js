a54a9cea66f19ee67009790d0b8a159fc42e7fe7
Karim <markhker@gmail.com>
2023-07-21 14:09:05 -0600

    use web crypto

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 /**
  * Generate password from allowed word
  */
-const crypto = require('crypto')
-
 const digits = '0123456789'
 const lowerCaseAlphabets = 'abcdefghijklmnopqrstuvwxyz'
 const upperCaseAlphabets = lowerCaseAlphabets.toUpperCase()
 const specialChars = '#!&@'
+
+function getRandomInt(min, max) {
+  const range = max - min;
+
+  const randomBytes = new Uint8Array(1);
+  crypto.getRandomValues(randomBytes);
+  const randomNumber = randomBytes[0];
+
+  const randomInt = Math.floor((randomNumber / 256) * (range + 1)) + min;
+
+  return randomInt;
+}
 
 module.exports = {
   /**
@@ -18,28 +28,28 @@ module.exports = {
    * @param  {boolean} options.upperCaseAlphabets Default: `true` true value includes uppercase alphabets in OTP
    * @param  {boolean} options.specialChars Default: `true` true value includes specialChars in OTP
    */
-  generate: function (length, options) {
-    length = length || 10
-    const generateOptions = options || {}
+  generate(length = 10, options = {}) {
+    const {
+      digits: optDigits = true,
+      lowerCaseAlphabets: optLowerCase = true,
+      upperCaseAlphabets: optUpperCase = true,
+      specialChars: optSpecialChars = true,
+    } = options;
 
-    generateOptions.digits = Object.prototype.hasOwnProperty.call(generateOptions, 'digits') ? options.digits : true
-    generateOptions.lowerCaseAlphabets = Object.prototype.hasOwnProperty.call(generateOptions, 'lowerCaseAlphabets') ? options.lowerCaseAlphabets : true
-    generateOptions.upperCaseAlphabets = Object.prototype.hasOwnProperty.call(generateOptions, 'upperCaseAlphabets') ? options.upperCaseAlphabets : true
-    generateOptions.specialChars = Object.prototype.hasOwnProperty.call(generateOptions, 'specialChars') ? options.specialChars : true
+    const allowsChars =
+      (optDigits ? digits : '') +
+      (optLowerCase ? lowerCaseAlphabets : '') +
+      (optUpperCase ? upperCaseAlphabets : '') +
+      (optSpecialChars ? specialChars : '');
 
-    const allowsChars = ((generateOptions.digits || '') && digits) +
-      ((generateOptions.lowerCaseAlphabets || '') && lowerCaseAlphabets) +
-      ((generateOptions.upperCaseAlphabets || '') && upperCaseAlphabets) +
-      ((generateOptions.specialChars || '') && specialChars)
     let password = ''
     while (password.length < length) {
-      const charIndex = crypto.randomInt(0, allowsChars.length)
-      if (password.length === 0 && generateOptions.digits === true && allowsChars[charIndex] === '0') {
+      const charIndex = getRandomInt(0, allowsChars.length)
+      if (password.length === 0 && optDigits && allowsChars[charIndex] === '0') {
         continue
       }
       password += allowsChars[charIndex]
     }
     return password
   }
-
 }
